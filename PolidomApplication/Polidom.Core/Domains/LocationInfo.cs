a53d93529ef58e8c59010e7d8180fdea9367bcba
JorgeTerrero <jorgedavidterreri@gmail.com>
2021-11-06 08:59:03 -0400

    add some properties to location domain

--- PolidomApplication/Polidom.Core/Domains/LocationInfo.cs
+++ PolidomApplication/Polidom.Core/Domains/LocationInfo.cs
@@ -35,5 +35,35 @@ namespace Polidom.Core.Domains
         /// </summary>
         [Column(TypeName = "decimal(18,7)")]
         public decimal Latitude { get; set; }
+        
+        /// <summary>
+        /// Get or set location's country
+        /// </summary>
+        public string Country { get; set; }
+
+        /// <summary>
+        /// Get or set location's locality
+        /// </summary>
+        public string Locality { get; set; }
+
+        /// <summary>
+        /// Get or set location's province.
+        /// </summary>
+        public string Province { get; set; }
+
+        /// <summary>
+        /// Get or set location's sector
+        /// </summary>
+        public string Sector { get; set; }
+
+        /// <summary>
+        /// Get or set location's direction
+        /// </summary>
+        public string Direction { get; set; }
+
+        /// <summary>
+        /// Get or set location's zip code.
+        /// </summary>
+        public string ZipCode { get; set; }
     }
 }
